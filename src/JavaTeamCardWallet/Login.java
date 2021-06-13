c83a1987fd70648d1e53bb42938173b69cde09b1
whals <whalswl0110@gmail.com>
2021-06-13 16:54:10 +0900

    name icon

--- src/JavaTeamCardWallet/Login.java
+++ src/JavaTeamCardWallet/Login.java
@@ -30,21 +30,21 @@ public class Login extends JFrame{
 		setTitle("Card Wallet");
 		
 		JLabel cardImageLabel = new JLabel("");
-		cardImageLabel.setBounds(260, 38, 480, 332);
+		cardImageLabel.setBounds(368, 39, 304, 332);
 		ImageIcon img= new ImageIcon ("C:/work/image/card.jpg");
-		cardImageLabel.setIcon(new ImageIcon(Login.class.getResource("/JavaTeamCardWallet/image/\uADF8\uB9BC1.jpg")));
+		cardImageLabel.setIcon(new ImageIcon(Login.class.getResource("/JavaTeamCardWallet/image/icon.PNG")));
 		
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("ID : ");
-		label.setBounds(405, 408, 47, 21);
+		label.setBounds(405, 393, 47, 21);
 		JLabel pswrd = new JLabel("Password : ");
-		pswrd.setBounds(387, 452, 80, 15);
+		pswrd.setBounds(387, 437, 80, 15);
 		JTextField txtID = new JTextField(10);
-		txtID.setBounds(498, 408, 132, 21);
+		txtID.setBounds(498, 393, 132, 21);
 		JPasswordField txtPass = new JPasswordField(10);
-		txtPass.setBounds(498, 449, 132, 21);
+		txtPass.setBounds(498, 434, 132, 21);
 		JButton btn1 = new JButton("Login");
-		btn1.setBounds(445, 490, 80, 23);
+		btn1.setBounds(445, 500, 80, 23);
 		panel.setLayout(null);
 		
 		panel.add(label);
